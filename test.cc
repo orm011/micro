3066733a35aa7e653d70f43eb3174408832aa8db
Oscar <orm@mit.edu>
2016-02-05 00:22:38 -0500

    control alignment explicitly. weird perf regression for both versions

--- test.cc
+++ test.cc
@@ -14,148 +14,148 @@ streams
  size_t num_columns,
  size_t column_size,
  int *__restrict__ * __restrict__ input_data,
- int *__restrict__ * __restrict__ results,
+ int *__restrict__ results,
  int concurrent
 )
 {
   if (concurrent) {
   for (size_t i = 0; i < column_size; ++i ) {
-      results[0][i] = input_data[0][i]; // gather
+      results[0] ^= input_data[0][i]; // gather
       
-      results[1][i] = input_data[1][i]; // gather
+      results[1] ^= input_data[1][i]; // gather
 
-      results[2][i] = input_data[2][i]; // gather
+      results[2] ^= input_data[2][i]; // gather
 
-      results[3][i] = input_data[3][i]; // gather
+      results[3] ^= input_data[3][i]; // gather
 
-      results[4][i] = input_data[4][i]; // gather
+      results[4] ^= input_data[4][i]; // gather
 
-      results[5][i] = input_data[5][i]; // gather
+      results[5] ^= input_data[5][i]; // gather
 
-      results[6][i] = input_data[6][i]; // gather
+      results[6] ^= input_data[6][i]; // gather
 
-      results[7][i] = input_data[7][i]; // gather
+      results[7] ^= input_data[7][i]; // gather
 
-      results[8 + 0][i] = input_data[8 + 0][i]; // gather
+      results[8 + 0] ^= input_data[8 + 0][i]; // gather
       
-      results[8 + 1][i] = input_data[8 + 1][i]; // gather
+      results[8 + 1] ^= input_data[8 + 1][i]; // gather
 
-      results[8 + 2][i] = input_data[8 + 2][i]; // gather
+      results[8 + 2] ^= input_data[8 + 2][i]; // gather
 
-      results[8 + 3][i] = input_data[8 + 3][i]; // gather
+      results[8 + 3] ^= input_data[8 + 3][i]; // gather
 
-      results[8 + 4][i] = input_data[8 + 4][i]; // gather
+      results[8 + 4] ^= input_data[8 + 4][i]; // gather
 
-      results[8 + 5][i] = input_data[8 + 5][i]; // gather
+      results[8 + 5] ^= input_data[8 + 5][i]; // gather
 
-      results[8 + 6][i] = input_data[8 + 6][i]; // gather
+      results[8 + 6] ^= input_data[8 + 6][i]; // gather
 
-      results[8 + 7][i] = input_data[8 + 7][i]; // gather
+      results[8 + 7] ^= input_data[8 + 7][i]; // gather
 
 
-      results[0 + 16][i] = input_data[0 + 16][i]; // gather
+      // results[0 + 16] ^= input_data[0 + 16][i]; // gather
       
-      results[1 + 16][i] = input_data[1 + 16][i]; // gather
+      // results[1 + 16] ^= input_data[1 + 16][i]; // gather
 
-      results[2 + 16][i] = input_data[2 + 16][i]; // gather
+      // results[2 + 16] ^= input_data[2 + 16][i]; // gather
 
-      results[3 + 16][i] = input_data[3 + 16][i]; // gather
+      // results[3 + 16] ^= input_data[3 + 16][i]; // gather
 
-      results[4 + 16][i] = input_data[4 + 16][i]; // gather
+      // results[4 + 16] ^= input_data[4 + 16][i]; // gather
 
-      results[5 + 16][i] = input_data[5 + 16][i]; // gather
+      // results[5 + 16] ^= input_data[5 + 16][i]; // gather
 
-      results[6 + 16][i] = input_data[6 + 16][i]; // gather
+      // results[6 + 16] ^= input_data[6 + 16][i]; // gather
 
-      results[7 + 16][i] = input_data[7 + 16][i]; // gather
+      // results[7 + 16] ^= input_data[7 + 16][i]; // gather
 
-      results[8 + 0 + 16][i] = input_data[8 + 0 + 16][i]; // gather
+      // results[8 + 0 + 16] ^= input_data[8 + 0 + 16][i]; // gather
       
-      results[8 + 1 + 16][i] = input_data[8 + 1 + 16][i]; // gather
+      // results[8 + 1 + 16] ^= input_data[8 + 1 + 16][i]; // gather
 
-      results[8 + 2 + 16][i] = input_data[8 + 2 + 16][i]; // gather
+      // results[8 + 2 + 16] ^= input_data[8 + 2 + 16][i]; // gather
 
-      results[8 + 3 + 16][i] = input_data[8 + 3 + 16][i]; // gather
+      // results[8 + 3 + 16] ^= input_data[8 + 3 + 16][i]; // gather
 
-      results[8 + 4 + 16][i] = input_data[8 + 4 + 16][i]; // gather
+      // results[8 + 4 + 16] ^= input_data[8 + 4 + 16][i]; // gather
 
-      results[8 + 5 + 16][i] = input_data[8 + 5 + 16][i]; // gather
+      // results[8 + 5 + 16] ^= input_data[8 + 5 + 16][i]; // gather
 
-      results[8 + 6 + 16][i] = input_data[8 + 6 + 16][i]; // gather
+      // results[8 + 6 + 16] ^= input_data[8 + 6 + 16][i]; // gather
 
-      results[8 + 7 + 16][i] = input_data[8 + 7 + 16][i]; // gather
+      // results[8 + 7 + 16] ^= input_data[8 + 7 + 16][i]; // gather
 
 
-      results[32 + 0][i] = input_data[32 + 0][i]; // gather
+      // results[32 + 0] ^= input_data[32 + 0][i]; // gather
       
-      results[32 + 1][i] = input_data[32+ 1][i]; // gather
+      // results[32 + 1] ^= input_data[32+ 1][i]; // gather
 
-      results[32 + 2][i] = input_data[32+ 2][i]; // gather
+      // results[32 + 2] ^= input_data[32+ 2][i]; // gather
 
-      results[32 + 3][i] = input_data[32+ 3][i]; // gather
+      // results[32 + 3] ^= input_data[32+ 3][i]; // gather
 
-      results[32 + 4][i] = input_data[32+ 4][i]; // gather
+      // results[32 + 4] ^= input_data[32+ 4][i]; // gather
 
-      results[32 + 5][i] = input_data[32+ 5][i]; // gather
+      // results[32 + 5] ^= input_data[32+ 5][i]; // gather
 
-      results[32 + 6][i] = input_data[32+ 6][i]; // gather
+      // results[32 + 6] ^= input_data[32+ 6][i]; // gather
 
-      results[32 + 7][i] = input_data[32+ 7][i]; // gather
+      // results[32 + 7] ^= input_data[32+ 7][i]; // gather
 
-      results[32 + 8 + 0][i] = input_data[32 + 8 + 0][i]; // gather
+      // results[32 + 8 + 0] ^= input_data[32 + 8 + 0][i]; // gather
       
-      results[32 + 8 + 1][i] = input_data[32 + 8 + 1][i]; // gather
+      // results[32 + 8 + 1] ^= input_data[32 + 8 + 1][i]; // gather
 
-      results[32 + 8 + 2][i] = input_data[32 + 8 + 2][i]; // gather
+      // results[32 + 8 + 2] ^= input_data[32 + 8 + 2][i]; // gather
 
-      results[32 + 8 + 3][i] = input_data[32 + 8 + 3][i]; // gather
+      // results[32 + 8 + 3] ^= input_data[32 + 8 + 3][i]; // gather
 
-      results[32 + 8 + 4][i] = input_data[32 + 8 + 4][i]; // gather
+      // results[32 + 8 + 4] ^= input_data[32 + 8 + 4][i]; // gather
 
-      results[32 + 8 + 5][i] = input_data[32 + 8 + 5][i]; // gather
+      // results[32 + 8 + 5] ^= input_data[32 + 8 + 5][i]; // gather
 
-      results[32 + 8 + 6][i] = input_data[32 + 8 + 6][i]; // gather
+      // results[32 + 8 + 6] ^= input_data[32 + 8 + 6][i]; // gather
 
-      results[32 + 8 + 7][i] = input_data[32 + 8 + 7][i]; // gather
+      // results[32 + 8 + 7] ^= input_data[32 + 8 + 7][i]; // gather
 
 
-      results[32 + 0 + 16][i] = input_data[32 + 0 + 16][i]; // gather
+      // results[32 + 0 + 16] ^= input_data[32 + 0 + 16][i]; // gather
       
-      results[32 + 1 + 16][i] = input_data[32 + 1 + 16][i]; // gather
+      // results[32 + 1 + 16] ^= input_data[32 + 1 + 16][i]; // gather
 
-      results[32 + 2 + 16][i] = input_data[32 + 2 + 16][i]; // gather
+      // results[32 + 2 + 16] ^= input_data[32 + 2 + 16][i]; // gather
 
-      results[32 + 3 + 16][i] = input_data[32 + 3 + 16][i]; // gather
+      // results[32 + 3 + 16] ^= input_data[32 + 3 + 16][i]; // gather
 
-      results[32 + 4 + 16][i] = input_data[32 + 4 + 16][i]; // gather
+      // results[32 + 4 + 16] ^= input_data[32 + 4 + 16][i]; // gather
 
-      results[32 + 5 + 16][i] = input_data[32 + 5 + 16][i]; // gather
+      // results[32 + 5 + 16] ^= input_data[32 + 5 + 16][i]; // gather
 
-      results[32 + 6 + 16][i] = input_data[32 + 6 + 16][i]; // gather
+      // results[32 + 6 + 16] ^= input_data[32 + 6 + 16][i]; // gather
 
-      results[32 + 7 + 16][i] = input_data[32 + 7 + 16][i]; // gather
+      // results[32 + 7 + 16] ^= input_data[32 + 7 + 16][i]; // gather
 
-      results[32 + 8 + 0 + 16][i] = input_data[32 + 8 + 0 + 16][i]; // gather
+      // results[32 + 8 + 0 + 16] ^= input_data[32 + 8 + 0 + 16][i]; // gather
       
-      results[32 + 8 + 1 + 16][i] = input_data[32 + 8 + 1 + 16][i]; // gather
+      // results[32 + 8 + 1 + 16] ^= input_data[32 + 8 + 1 + 16][i]; // gather
 
-      results[32 + 8 + 2 + 16][i] = input_data[32 + 8 + 2 + 16][i]; // gather
+      // results[32 + 8 + 2 + 16] ^= input_data[32 + 8 + 2 + 16][i]; // gather
 
-      results[32 + 8 + 3 + 16][i] = input_data[32 + 8 + 3 + 16][i]; // gather
+      // results[32 + 8 + 3 + 16] ^= input_data[32 + 8 + 3 + 16][i]; // gather
 
-      results[32 + 8 + 4 + 16][i] = input_data[32 + 8 + 4 + 16][i]; // gather
+      // results[32 + 8 + 4 + 16] ^= input_data[32 + 8 + 4 + 16][i]; // gather
 
-      results[32 + 8 + 5 + 16][i] = input_data[32 + 8 + 5 + 16][i]; // gather
+      // results[32 + 8 + 5 + 16] ^= input_data[32 + 8 + 5 + 16][i]; // gather
 
-      results[32 + 8 + 6 + 16][i] = input_data[32 + 8 + 6 + 16][i]; // gather
+      // results[32 + 8 + 6 + 16] ^= input_data[32 + 8 + 6 + 16][i]; // gather
 
-      results[32 + 8 + 7 + 16][i] = input_data[32 + 8 + 7 + 16][i]; // gather
+      // results[32 + 8 + 7 + 16] ^= input_data[32 + 8 + 7 + 16][i]; // gather
       
   }
   } else {
     for (size_t arr = 0; arr < num_columns; ++arr) {
       for (size_t i = 0; i < column_size; ++i ) {
-	results[arr][i] = input_data[arr][i]; // gather
+	results[arr] ^= input_data[arr][i]; // gather
       }
     }
   }
@@ -182,19 +182,40 @@ void serialstreams
 */
 
 void run(benchmark::State & state, int concurrent){
-  int ** columns = new int*[state.range_x()] {};
-  int ** results = new int*[state.range_x()] {};
+
+  const int num_vectors = state.range_x();
+  const size_t vector_size_in_ints = state.range_y();
+  const int cache_line_size_in_ints = 64/(sizeof(int)); // in ints. assuming 64 bytes in cache line.
+
+  int ** columns = new int*[num_vectors] {};
+  int *results = new int[num_vectors] {};
+
+  // allocate enough space for all vectors, as well as some space to spread them out
+  size_t total_size_ints = num_vectors * (vector_size_in_ints + cache_line_size_in_ints);
+  size_t total_size_bytes = total_size_ints * sizeof(int);
   
-  for (int i =0; i < state.range_x(); ++i) {
-    columns[i] = new int[state.range_y()];
-    memset(columns[i], 'x', state.range_y());
-    results[i] = new int[state.range_y()];
+  int * all_cols = (int*)aligned_alloc(64, total_size_bytes);
+  assert(all_cols != nullptr);
+
+  int * start_offset = all_cols;
+  for (int i =0; i < num_vectors; ++i) {
+    columns[i] = start_offset;
+    memset(columns[i], 'x', vector_size_in_ints*sizeof(int));
+
+    start_offset += (vector_size_in_ints + cache_line_size_in_ints);
   }
 
-  char *p = nullptr;
-  scanf("%m[a-z]", &p);
-  free(p);
-  
+  // char *p = nullptr;
+  // size_t size = 0;
+  // printf("press enter to continue...\n");
+  // auto res = getline(&p, &size, stdin);
+  // if (res == -1){
+  //   perror("error at getline\n");
+  //   exit(1);
+  // } else {
+  //   printf("continuing...\n");
+  // }
+
   while (state.KeepRunning()) {
 //     PCM * m = PCM::getInstance();
 //     m->resetPMU();
@@ -235,11 +256,7 @@ void run(benchmark::State & state, int concurrent){
     // state.ResumeTiming();
   }
 
-  for (int i =0; i < state.range_x(); ++i) {
-    delete[] columns[i];
-    delete[] results[i];
-  }
-
+  free(all_cols);
   delete[] columns;
   delete[] results;
 }
@@ -252,9 +269,10 @@ static void BM_serial(benchmark::State& state) {
   run(state, 0);
 }
 
+int number = 16;
+int sizes = (1<<27); //+ (number*16); // enough cache lines for staggering
 
 // Register the function as a benchmark
-BENCHMARK(BM_serial)->RangePair(64, 64, 1<<26, 1<<26);
-BENCHMARK(BM_concurrent)->RangePair(64, 64, 1<<26, 1<<26);
-
+BENCHMARK(BM_serial)->RangePair(number, number, sizes, sizes);
+BENCHMARK(BM_concurrent)->RangePair(number, number, sizes, sizes);
 BENCHMARK_MAIN();
