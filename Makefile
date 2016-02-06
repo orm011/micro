df561066ab8ec3c3b19892f50d1891c8eb34aad4
Oscar <orm@mit.edu>
2016-02-05 19:58:55 -0500

    gather benchmark added

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 all:	test.cc
 	clang++ -g -Wall --std=c++14 -O3 test.cc -lbenchmark -lpthread -o test -I/home/orm/IntelPerformanceCounterMonitor-PCM-V2.10/ -L/home/orm/IntelPerformanceCounterMonitor-PCM-V2.10/intelpcm.so/ -Wl,-rpath=/home/orm/IntelPerformanceCounterMonitor-PCM-V2.10/intelpcm.so/  -lintelpcm
+
+debug:	test.cc
+	clang++ -g -Wall --std=c++14 -O0 test.cc -lbenchmark -lpthread -o test -I/home/orm/IntelPerformanceCounterMonitor-PCM-V2.10/ -L/home/orm/IntelPerformanceCounterMonitor-PCM-V2.10/intelpcm.so/ -Wl,-rpath=/home/orm/IntelPerformanceCounterMonitor-PCM-V2.10/intelpcm.so/  -lintelpcm
--- test.cc
+++ test.cc
@@ -5,16 +5,18 @@
 #include <iostream>
 #include <immintrin.h>
 #include <iterator>
+#include <algorithm>
 
 //_mm256_stream_si256 
 using namespace std;
-const int k_num_columns = 16;
-const int sizes = (1<<27); 
+const int k_num_columns = 2;
+const int k_sizes = (1<<27); 
+const int k_check_output = true;
 
 void
-gather_together(
+gather_interleaved(
  size_t column_size,
- size_t *__restrict__ gather_positions,
+ uint32_t*__restrict__ gather_positions,
  int *__restrict__ * __restrict__ input_data,
  int *__restrict__ * __restrict__ output)
 {
@@ -26,9 +28,9 @@ gather_together(
 }
 
 void
-gather_separately(
+gather_series(
  size_t column_size,
- size_t *__restrict__ gather_positions,
+ uint32_t *__restrict__ gather_positions, // use 32bit ints as offsets
  int *__restrict__ * __restrict__ input_data,
  int *__restrict__ * __restrict__ output)
 {
@@ -45,100 +47,151 @@ gather_separately(
 
 
 void
-merge_and_gather(
+zip_gather_project(
  size_t column_size,
- size_t *__restrict__ gather_positions,
+ uint32_t*__restrict__ gather_positions,
  int *__restrict__ * __restrict__ input_data,
  int *__restrict__ * __restrict__ output)
 {
   struct int_pair {int a; int b;};
   int_pair * merged = new int_pair[column_size];
-  int_pair * merged_output = new int_pair[column_size];
   
-  //merge
+  //zip and materialize
   for (size_t pos = 0; pos < column_size; ++pos) {
     merged[pos].a = input_data[0][pos];
     merged[pos].b = input_data[1][pos];
   }
 
-  // gather once
+  //gather and project in the same loop
   for (size_t pos = 0; pos < column_size; ++pos) {
-    merged_output[pos] = merged[gather_positions[pos]];
-  }
-
-  //split
-  for (size_t pos = 0; pos < column_size; ++pos) {
-    output[0][pos] = merged_output[pos].a;
-    output[1][pos] = merged_output[pos].b;
+    size_t gpos = gather_positions[pos];
+    
+    output[0][pos] = merged[gpos].a;
+    output[1][pos] = merged[gpos].b;
   }
 }
 
 void
-streams
+aggregate_series
 (
  size_t column_size,
  int *__restrict__ * __restrict__ input_data,
- int *__restrict__ results,
- int concurrent
+ int *__restrict__ results
 )
 {
-  if (concurrent) {
-  for (size_t i = 0; i < column_size; ++i ) {
-      results[0] ^= input_data[0][i]; // gather
-      
-      results[1] ^= input_data[1][i]; // gather
-
-      results[2] ^= input_data[2][i]; // gather
-
-      results[3] ^= input_data[3][i]; // gather
-
-      results[4] ^= input_data[4][i]; // gather
-
-      results[5] ^= input_data[5][i]; // gather
-
-      results[6] ^= input_data[6][i]; // gather
-
-      results[7] ^= input_data[7][i]; // gather
-
-      results[8 + 0] ^= input_data[8 + 0][i]; // gather
-      
-      results[8 + 1] ^= input_data[8 + 1][i]; // gather
-
-      results[8 + 2] ^= input_data[8 + 2][i]; // gather
-
-      results[8 + 3] ^= input_data[8 + 3][i]; // gather
-
-      results[8 + 4] ^= input_data[8 + 4][i]; // gather
-
-      results[8 + 5] ^= input_data[8 + 5][i]; // gather
-
-      results[8 + 6] ^= input_data[8 + 6][i]; // gather
-
-      results[8 + 7] ^= input_data[8 + 7][i]; // gather
+  for (size_t arr = 0; arr < k_num_columns; ++arr) {
+    for (size_t i = 0; i < column_size; ++i ) {
+      results[arr] ^= input_data[arr][i]; // gather
+    }
   }
-  } else {
-    for (size_t arr = 0; arr < k_num_columns; ++arr) {
-      for (size_t i = 0; i < column_size; ++i ) {
-	results[arr] ^= input_data[arr][i]; // gather
+}
+
+
+void
+aggregate_interleaved
+(
+ size_t column_size,
+ int *__restrict__ * __restrict__ input_data,
+ int *__restrict__ results
+)
+{
+  for (size_t i = 0; i < column_size; ++i ) {
+      results[0] ^= input_data[0][i];  
+      
+      results[1] ^= input_data[1][i];  
+
+      results[2] ^= input_data[2][i];  
+
+      results[3] ^= input_data[3][i];  
+
+      results[4] ^= input_data[4][i];  
+
+      results[5] ^= input_data[5][i];  
+
+      results[6] ^= input_data[6][i];  
+
+      results[7] ^= input_data[7][i];  
+
+      results[8 + 0] ^= input_data[8 + 0][i];  
+      
+      results[8 + 1] ^= input_data[8 + 1][i];  
+
+      results[8 + 2] ^= input_data[8 + 2][i];  
+
+      results[8 + 3] ^= input_data[8 + 3][i];  
+
+      results[8 + 4] ^= input_data[8 + 4][i];  
+
+      results[8 + 5] ^= input_data[8 + 5][i];  
+
+      results[8 + 6] ^= input_data[8 + 6][i];  
+
+      results[8 + 7] ^= input_data[8 + 7][i];  
+  }
+}
+
+template <typename T> void run_full(benchmark::State & state, T func){
+  assert(k_sizes == state.range_y());
+  assert(k_num_columns == state.range_x());
+  
+  const int num_vectors = state.range_x();
+  const size_t vector_size_in_ints = state.range_y();
+
+  int ** columns = new int*[num_vectors] {};
+  int ** outputs = new int*[num_vectors] {};
+  uint32_t * positions =  new uint32_t[vector_size_in_ints];
+
+  for (size_t i = 0; i < vector_size_in_ints; ++i) {
+    positions[i] = i;
+  }
+
+  std::srand(1000);
+  std::random_shuffle(positions, positions + vector_size_in_ints);// WARNING: this may pollute perf counters. (but not timing)
+
+  for (int i = 0; i < num_vectors; ++i) {
+    columns[i] = new int[vector_size_in_ints];
+
+    for (int j = 0; j < vector_size_in_ints; ++j) {
+      columns[i][j] = (i ^ j);
+    }
+    
+    outputs[i] = new int[vector_size_in_ints];
+  }
+
+  while (state.KeepRunning()) {
+    func(state.range_y(), positions, columns, outputs);
+  }
+
+  if (k_check_output) {
+    for (int i = 0; i < num_vectors; ++i) {
+      for (int j = 0; j < vector_size_in_ints; ++j) {
+	int original_j = (outputs[i][j] ^ i);
+	assert(positions[j] == original_j);
       }
     }
   }
-
   
+  for (int i = 0; i < num_vectors; ++i) {
+    delete[] columns[i];
+    delete[] outputs[i];
+  }
+  
+  delete[] columns;
+  delete[] outputs;
 }
 
-void run(benchmark::State & state, int concurrent){
+template <typename T> void run(benchmark::State & state, T func) {
 
   const int num_vectors = state.range_x();
   const size_t vector_size_in_ints = state.range_y();
-  const int cache_line_size_in_ints = 64/(sizeof(int)); // in ints. assuming 64 bytes in cache line.
+  //const int cache_line_size_in_ints = 64/(sizeof(int)); // in ints. assuming 64 bytes in cache line.
 
   int ** columns = new int*[num_vectors] {};
   int *results = new int[num_vectors] {};
 
   // allocate enough space for all vectors, as well as some space to spread them out
-  size_t total_size_ints = num_vectors * (vector_size_in_ints + cache_line_size_in_ints);
-  size_t total_size_bytes = total_size_ints * sizeof(int);
+  //size_t total_size_ints = num_vectors * (vector_size_in_ints + cache_line_size_in_ints);
+  //size_t total_size_bytes = total_size_ints * sizeof(int);
   
   //int * all_cols = (int*)aligned_alloc(64, total_size_bytes);
   //assert(all_cols != nullptr);
@@ -148,59 +201,11 @@ void run(benchmark::State & state, int concurrent){
     //columns[i] = start_offset;
     columns[i] = new int[vector_size_in_ints];
     memset(columns[i], 'x', vector_size_in_ints*sizeof(int));
-
     //start_offset += (vector_size_in_ints + cache_line_size_in_ints);
   }
 
-  // char *p = nullptr;
-  // size_t size = 0;
-  // printf("press enter to continue...\n");
-  // auto res = getline(&p, &size, stdin);
-  // if (res == -1){
-  //   perror("error at getline\n");
-  //   exit(1);
-  // } else {
-  //   printf("continuing...\n");
-  // }
-
   while (state.KeepRunning()) {
-//     PCM * m = PCM::getInstance();
-//     m->resetPMU();
-//     auto status = m->program (PCM::DEFAULT_EVENTS, NULL);
-
-//     switch (status)
-//       {
-//       case PCM::Success:
-// 	break;
-//       case PCM::MSRAccessDenied:
-// 	cerr << "Access to Intel(r) Performance Counter Monitor has denied (no MSR or PCI CFG space access)." << endl;
-// 	exit(EXIT_FAILURE);
-//       case PCM::PMUBusy:
-// 	cerr << "Access to Intel(r) Performance Counter Monitor has denied (Performance Monitoring Unit is occupied by other application). Try to stop the application that use\
-// s PMU." << endl;
-// 	cerr << "Alternatively you can try running Intel PCM with option -r to reset PMU configuration at your own risk." << endl;
-// 	exit(EXIT_FAILURE);
-//       default:
-// 	cerr << "Access to Intel(r) Performance Counter Monitor has denied (Unknown error)." << endl;
-// 	exit(EXIT_FAILURE);
-//       }
-
-//     cerr << "\nDetected " << m->getCPUBrandString() << " \"Intel(r) microarchitecture codename " << m->getUArchCodename() << "\"" << endl;
-    
-
-//     SystemCounterState before_sstate = getSystemCounterState();
-//    state.ResumeTiming();
-    streams(state.range_y(), columns, results, concurrent);
-    // state.PauseTiming();
-
-    // SystemCounterState after_sstate = getSystemCounterState();
-    // auto ratio = getL3CacheHitRatio(before_sstate, after_sstate);
-    // auto hits = getL3CacheHits(before_sstate, after_sstate);
-    // auto misses = getL3CacheMisses(before_sstate, after_sstate);
-
-    // printf("hits:  %ul\n", hits);
-    // printf("misses:  %ul\n", misses);
-    // state.ResumeTiming();
+    func(state.range_y(), columns, results);
   }
 
   //free(all_cols);
@@ -212,17 +217,32 @@ void run(benchmark::State & state, int concurrent){
   delete[] results;
 }
 
-static void BM_concurrent(benchmark::State& state) {
-  run(state, 1);
+static void BM_aggregate_interleaved(benchmark::State& state) {
+  run(state, aggregate_interleaved);
 }
 
-static void BM_serial(benchmark::State& state) {
-  run(state, 0);
+static void BM_aggregate_series(benchmark::State& state) {
+  run(state, aggregate_series);
 }
 
+static void BM_gather_series(benchmark::State& state) {
+  run_full(state, gather_series);
+}
+
+static void BM_gather_interleaved(benchmark::State& state) {
+  run_full(state, gather_interleaved);
+}
+
+static void BM_zip_gather_project(benchmark::State& state) {
+  run_full(state, zip_gather_project);
+}
 
 
 // Register the function as a benchmark
-BENCHMARK(BM_serial)->RangePair(k_num_columns, k_num_columns, sizes, sizes);
-BENCHMARK(BM_concurrent)->RangePair(k_num_columns, k_num_columns, sizes, sizes);
+BENCHMARK(BM_aggregate_series)->ArgPair(k_num_columns, k_sizes);
+BENCHMARK(BM_aggregate_interleaved)->ArgPair(k_num_columns, k_sizes);
+BENCHMARK(BM_gather_series)->ArgPair(k_num_columns, k_sizes);
+BENCHMARK(BM_gather_interleaved)->ArgPair(k_num_columns, k_sizes);
+BENCHMARK(BM_zip_gather_project)->ArgPair(k_num_columns, k_sizes);
+
 BENCHMARK_MAIN();
